da389c4fad3fabd443748ae60cdbea1583551941
LuigiPantas <luigi.pantas@gmail.com>
2025-02-03 13:17:38 -0300

    opção de saida
    incluso no sistema a opção de saida para o usuário

--- cartotio_ebac.cpp
+++ cartotio_ebac.cpp
@@ -124,6 +124,7 @@ int main()
 		printf("\t1 - Registrar nomesn\n");
 		printf("\t2 - Consultar nomes\n");
 		printf("\t3 - Deletar nomes\n");
+		printf("\t4 - Sair do sistema\n\n");
 		printf("Opção ");//fim do menu
 	
 		scanf("%d", &opcao); // armazenamento a escolha do usuário
@@ -143,6 +144,12 @@ int main()
 			case 3:
 			deletar();
 			break;
+			
+			case 4:
+			printf("Obrigado por utilizar o sistema!\n");
+			return 0;
+			break;
+			
 			
 			default:
 			printf("Essa opção não está disponivel\n");
